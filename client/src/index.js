b7e86857885f3ba6fb488c38956a786f31ff936f
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-11-15 10:50:41 -0700

    commiting mvp

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import BrowserRouter as Router, Route, NavLink from 'react-router-dom';
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Router><App /></Router>, rootElement);
+ReactDOM.render(<App />, rootElement);
