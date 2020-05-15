0d5e5415dcce1adf08f45d5a29e67eade4c8b946
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2020-05-15 17:59:59 -0600

    added form to add a new color name and hex and styled the form

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import BubblePage from './components/BubblePage';
 import PrivateRoute from './utils/PrivateRoute.js';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import Login from './components/Login';
+import './styles.scss';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-      <Route exact path="/" render={props => <Login {...props} />} />
-        <PrivateRoute path='/bubbles' component={BubblePage} />
+      <div className='App'>
+        <Route exact path='/' render={(props) => <Login {...props} />} />
+        <PrivateRoute path='/protected' component={BubblePage} />
       </div>
     </Router>
   );
